430ccc3db44f11bb2df60a6693d8cf4b9caf1d56
nayeonkim <kny0628m@gmail.com>
2023-03-29 17:58:29 +0900

    230329 SWEA 1244

--- SWEA/0328/1244/sol.py
+++ SWEA/0328/1244/sol.py
@@ -1,42 +1,58 @@
 import sys
-
 sys.stdin = open('input.txt')
 
-
+# 숫자 리스트, 교환횟수
 def change(numbers, cnt):
+    # result = [[], []]
     global result
 
     temp = ''
+
+    # [1, 2, 3] 순회하면서 temp에 추가
     for number in numbers:
+        # temp == 123
         temp += number
 
+    # result[1] : result의 제일 마지막 자리에 123이 있다면 반환 후 종료
     if int(temp) in result[cnt]:
         return
+    # result[1] : 없다면 추가
+    # result[1] = 123
     else:
         result[cnt].append(int(temp))
 
     if cnt == 0:
         return
 
+    # n = 3
     n = len(numbers)
 
+    # 0부터 2까지 순회
     for i in range(n):
+        # 1부터 2까지 순회
         for j in range(i + 1, n):
+            # 바로 뒷 숫자랑 자리 바꿈
             numbers[i], numbers[j] = numbers[j], numbers[i]
 
+            # cnt자리를 왼쪽으로 한칸 옮겨서 다시 호출
             change(numbers, cnt - 1)
 
+            # 다시 자리 바꿈
             numbers[i], numbers[j] = numbers[j], numbers[i]
 
 
 T = int(input())
 
 for t in range(1, T + 1):
+    # 123, 1
     temp, cnt = input().split()
 
+    # numbers = ['1', '2', '3']
     numbers = list(temp)
+
+    # [[], []]
     result = [[] for _ in range(int(cnt) + 1)]
 
     change(numbers, int(cnt))
-
+    print(result)
     print('#{} {}'.format(t, max(result[0])))
--- SWEA/0328/1244/sol_mine.py
+++ SWEA/0328/1244/sol_mine.py
@@ -1,21 +1,32 @@
 import sys
 sys.stdin = open('input.txt')
 
-def perm(num, N):
-
-    res = ''
-    for n in num:
-        res += n
-
-    for j in range(i, k):
-        p[i], p[j] = p[j], p[i]
-        perm(num, N-1)
-        p[i], p[j] = p[j], p[i]
-
 T = int(input())
-for C in range(1, T+1):
+for tc in range(1, T+1):
     temp, N = input().split()
-    num = list(temp)
-    print(num)
 
-    perm(num, N)
+    N = int(N)
+    num = list(map(int, temp))
+
+    for _ in range(N):
+        maxV = 0
+        # 1부터 2까지를 기준으로 삼는다. 1일 때,
+        for i in range(len(num)-1):
+            # 2부터 3까지
+            for j in range(i+1, len(num)):
+                # 둘의 자리를 바꾼다. -> 213
+                num[i], num[j] = num[j], num[i]
+
+                # maxV 업데이트
+                res = ''
+                for l in range(len(num)):
+                    res += str(num[l])
+                if int(maxV) < int(res):
+                    maxV = int(res)
+
+                # 원 위치
+                num[i], num[j] = num[j], num[i]
+
+        num = list(map(int, str(maxV)))
+
+    print(f'#{tc} {maxV}')
