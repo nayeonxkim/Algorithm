3da7d6290ab86ace499315fc144fa88efb55b7db
NayeonKim <kny0628m@gmail.com>
2023-12-05 13:35:33 +0900

    인프런 탐색 끝

--- inflearn/탐색&시뮬레이션/격자판 회문수/sol.py
+++ inflearn/탐색&시뮬레이션/격자판 회문수/sol.py
@@ -1,11 +1,38 @@
+'''
+0,0 -> 0,6
+0,1 -> 1,6
+0,2 -> 2,6
+0,6 -> 6,6
+
+1,0 -> 0,5
+1,1 -> 1,5
+
+i, j = j, 6-i
+
+'''
 import sys
 sys.stdin = open('input.txt')
 
 arr = [list(map(int, input().split())) for _ in range(7)]
+cnt = 0
 
 # 1. 행
-for i in range(3):
+for i in range(7):
     for j in range(3):
-        if arr[i][j:j+5] == arr[i][j:j+5:-1]:
-            print(arr[i][j:j+5])
+        if arr[i][j:j+5] == list(reversed(arr[i][j:j+5])):
+            cnt += 1
+
+# 2. 열 우선 arr로 변경
+new_arr = [[0]*7 for _ in range(7)]
+for i in range(7):
+    for j in range(7):
+        new_arr[j][6-i] = arr[i][j]
+
+# 2. 열
+for i in range(7):
+    for j in range(3):
+        if new_arr[i][j:j+5] == list(reversed(new_arr[i][j:j+5])):
+            cnt += 1
+
+print(cnt)
 
