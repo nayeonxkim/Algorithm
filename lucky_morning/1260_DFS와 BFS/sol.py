8ba955c7f68cd90debda6b83b8b747221fab151a
NayeonKim <kny0628m@gmail.com>
2023-12-10 10:22:41 +0900

    럭키모닝 DFS & BFS 예제 3개 통과

--- lucky_morning/1260_DFS와 BFS/sol.py
+++ lucky_morning/1260_DFS와 BFS/sol.py
@@ -1,5 +1,6 @@
 from collections import deque
 import sys
+import copy
 sys.stdin = open('input.txt')
 
 # 1. 입력받기, 그래프 -> 2차원 배열
@@ -10,10 +11,12 @@ for _ in range(M):
     arr[i][j] = 1
     arr[j][i] = 1
 
+arr_bfs = copy.deepcopy(arr)
 
 # 2. DFS 탐색
 def DFS(arr, V):
     visited = [0] * (N+1)
+
     # 첫 출발점 담음
     queue = deque()
     queue.append(V)
@@ -27,7 +30,29 @@ def DFS(arr, V):
                 queue.append(j)
                 visited[j] = 1
                 ans.append(j)
-    print(ans)
+                break
+    print(*ans)
+
+# 2. DFS 탐색
+def BFS(arr, V):
+    visited = [0] * (N+1)
+
+    # 첫 출발점 담음
+    queue = deque()
+    queue.append(V)
+    ans = [V]
+    visited[V] = 1
+
+    while queue:
+        now = queue.popleft()
+        for j in range(N+1):
+            if visited[j] == 0 and arr[now][j] == 1:
+                queue.append(j)
+                visited[j] = 1
+                ans.append(j)
+
+    print(*ans)
+
 
 DFS(arr, V)
-
+BFS(arr_bfs, V)
