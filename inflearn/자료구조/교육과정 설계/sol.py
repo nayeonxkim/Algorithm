40529b7eab8765cd1c046c992db8b86692b45853
NayeonKim <kny0628m@gmail.com>
2023-12-19 23:43:05 +0900

    인프런 교육과정 설계 강의풀이

--- inflearn/자료구조/교육과정 설계/sol.py
+++ inflearn/자료구조/교육과정 설계/sol.py
@@ -2,38 +2,32 @@ import sys
 sys.stdin = open('input.txt')
 from collections import deque
 '''
-생성한 check라는 빈 리스트에 CBA만 넣으면서 검사
+큐 풀이
+필수과목 = 큐
+1. plan의 한 과목씩 순회하며 검사
+2. 현재 과목이 큐의 첫번째 과목과 일치하지 않는다면 바로 NO 반환
+3. 만약 순서대로 일치하여 for문이 끝난다면 큐의 길이를 확인
+4. 큐의 길이가 0이라면 모든 필수과목이 설계 된 상태 -> YES 반환
+5. 큐의 길이가 1이상이라면 하나 이상의 필수과목이 설계되지 않음 -> NO 반환
 
-CBDAGE
-res = [C B A]
-
-# 1. res에 현재 글자가 있다면 pass
-# 2. required[p]와 현재 글자가 같다면 res에 추가, p +1
-# 3. res에 있는 글자라면 함수 끝내고 NO 반환
-# 4. 전부 돌고 나서 res join 값이 need와 같아면 YES 다르면 NO
+** 필수과목이 두 번 나오는 경우도 순서만 일치한다면 YES를 반환해야하는데, NO를 반환하지 않나?
+큐를 사용하여 풀이하므로, 한 번 나온 과목은 popleft하여 큐에서 사라진 상태
+따라서 한 과목이 두 번째 나오면 이는 필수과목에 포함되지 않는 과목이라고 여겨짐
+따라서 문제없이 YES를 반환함
+=> 각 필수과목에 대해 단 한 번씩만 순서를 검사한다고 생각하면 됨
 '''
 
-required = input()
+need = input()
 N = int(input())
 for tc in range(1, N+1):
     plan = input()
-
-    res = deque()
-    p = 0
-    for sub in plan:
-        if sub in res:
-            pass
-        elif sub == required[p]:
-            res.append(sub)
-            if p < len(required)-1:
-                p += 1
-        elif sub in res:
-            print(f"#{tc} NO")
-            break
-    if ''.join(res) == required:
-        print(f"#{tc} YES")
+    dq = deque(need)
+    for x in plan:
+        if x in dq:
+            if x != dq.popleft():
+                print("NO")
     else:
-        print(f"#{tc} NO")
-
-
-
+        if len(dq) == 0:
+            print("YES")
+        else:
+            print("NO")
