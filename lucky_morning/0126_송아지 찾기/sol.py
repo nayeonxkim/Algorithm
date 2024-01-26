0003c63e71c0db1bccee1c024db9534def7eb0bd
NayeonKim <kny0628m@gmail.com>
2024-01-26 10:34:04 +0900

    주석 추가

--- lucky_morning/0126_송아지 찾기/sol.py
+++ lucky_morning/0126_송아지 찾기/sol.py
@@ -18,8 +18,10 @@ while queue:
 
     for m in [1, -1, 5]:
         next = now + m
+        # 다음 칸이 범위 내에 있고 방문한적 없으면 -> 현재 위치 + 1로 방문표시
         if 0 < next < len(visitied) and not visitied[next]:
             visitied[next] = visitied[now] + 1
             queue.append(next)
 
+# 도착점에 이미 방문하면 더이상 표시 X -> 가장 작은 수가 표시 됨
 print(visitied[E] - 1)
--- lucky_morning/0126_재귀함수를 이용한 이진수 출력/sol.py
+++ lucky_morning/0126_재귀함수를 이용한 이진수 출력/sol.py
@@ -1,7 +1,7 @@
 import sys
 sys.stdin = open('input.txt')
 
-
+# 재귀함수
 res = ''
 def toBinary(N):
     if N == 0:
@@ -11,8 +11,7 @@ def toBinary(N):
     res += str(N%2)
     toBinary(N//2)
 
-
-
+# 뒤집어서 답 출력
 N = int(input())
 toBinary(N)
 ans = list(reversed(res))
